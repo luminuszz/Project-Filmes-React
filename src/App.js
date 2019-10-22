449144022fc1d6f57f526ffde491d59f3803b738
davi ribeiro <luminuszz43@gmail.com>
2019-10-22 11:34:10 -0300

    Aplicando Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
-
+import './App.css'
+import Routes from './routes';
 
 class App extends Component{
   constructor(props){
@@ -13,7 +14,7 @@ class App extends Component{
   render(){
     return(
       <div className="app">
-       
+        <Routes/>
 
       </div>
     )
